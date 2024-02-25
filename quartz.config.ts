c03cf1193bd4fc34e9f80351306997b358b4f5df
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-02-25 02:00:26 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,7 +30,7 @@ const config: QuartzConfig = {
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
-          light: "#161618",
+          light: "#000000",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
