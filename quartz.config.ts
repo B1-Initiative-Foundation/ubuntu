e6df5d0513bd2190cd9e89d66081591ac852aa7c
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-02-25 01:47:53 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#621073",
+          light: "#5E3685",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
