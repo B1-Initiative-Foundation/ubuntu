d7067c2bb8634afad604f46c9d7b7403980b391b
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-02-25 20:41:58 -0600

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,7 +16,7 @@ export default ((opts?: Options) => {
         <hr />
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">This work is licensed under CC BY-NC-ND 4.0</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
