db2e039e4f9c3232c8a23a543ad6bce14573fd16
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-02-28 22:07:14 -0600

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,10 +47,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({
-  filterFn: (node) => {
-    const omit = new Set(["B1 INITIATIVE FOUNDATION", "DRAFTS", "hosting"])
-    return !omit.has(node.name.toLowerCase())
+    Component.DesktopOnly(Component.Explorer()
   }})),
   ],
   right: [],
