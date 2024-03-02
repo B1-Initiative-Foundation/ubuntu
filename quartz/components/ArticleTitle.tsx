2f77fbefba2c29223ce4ba9580bf81d13222f5f7
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-03-02 17:31:06 -0600

    Update ArticleTitle.tsx

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -4,7 +4,7 @@ import { classNames } from "../util/lang"
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
   if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+    return <h1 class={classNames("article-title")}>{title}</h1>
   } else {
     return null
   }
