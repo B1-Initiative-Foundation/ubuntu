3eae06880c5d0f59626cbad13df1c7e1a28e1a18
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-03-02 17:51:04 -0600

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,9 +35,8 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [],
+  beforeBody: [Component.ArticleTitle()],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
