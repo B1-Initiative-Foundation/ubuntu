0ae73516d3ec53c304571e7f1b00298fed790c56
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-03-02 20:42:30 -0600

    Update Graph.tsx

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -29,7 +29,7 @@ const defaultOptions: GraphOptions = {
     drag: true,
     zoom: true,
     depth: 1,
-    scale: 1.4,
+    scale: 3,
     repelForce: 0.5,
     centerForce: 0.3,
     linkDistance: 30,
