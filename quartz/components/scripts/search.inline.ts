89bfb91f60b39399e2913f76f6e7397afb49b44d
Brother Legend <115322500+bigbrolgnd@users.noreply.github.com>
2024-03-05 20:35:57 -0600

    Update search.inline.ts

--- quartz/components/scripts/search.inline.ts
+++ quartz/components/scripts/search.inline.ts
@@ -40,7 +40,7 @@ let index = new FlexSearch.Document<Item>({
 
 const p = new DOMParser()
 const fetchContentCache: Map<FullSlug, Element[]> = new Map()
-const contextWindowWords = 30
+const contextWindowWords = 10
 const numSearchResults = 8
 const numTagResults = 5
 
